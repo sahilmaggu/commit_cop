0063423657f691bf8f334c562fcdc11a2fd21785
sahilmaggu <sahil.maggu@yahoo.com>
2015-06-25 14:48:57 +0530

    Update CommitCop.java

--- src/main/java/com/intuit/commit/cop/CommitCop.java
+++ src/main/java/com/intuit/commit/cop/CommitCop.java
@@ -3,7 +3,7 @@ package com.intuit.commit.cop;
 public class CommitCop {
 
 	public static String welcome(){
-		String s="test1";
+		String s="test2";
 		
 		String message = "Hello World Sahil-6";
 		return message;
